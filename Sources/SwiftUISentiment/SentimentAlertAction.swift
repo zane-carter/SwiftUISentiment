0f7dfdccadda2179e7e80b0796757c5aa40f33d1
Zane Carter <carter.m.zane@gmail.com>
2022-09-09 11:30:05 +1000

    Updated name to label

--- Sources/SwiftUISentiment/SentimentAlertAction.swift
+++ Sources/SwiftUISentiment/SentimentAlertAction.swift
@@ -12,15 +12,15 @@ import SwiftUI
 public enum SentimentAlertAction: Identifiable {
     /// Open the Twitter app to the given account `handle`
     /// If the Twitter app is not available, the user is taken to the Twitter profile URL in safari
-    case twitter(_ name: String, handle: String)
+    case twitter(_ label: String, handle: String)
     /// Navigate the user to the given `url` in the the external Safari App
-    case website(_ name: String, url: URL)
+    case website(_ label: String, url: URL)
     /// Navigates the user to their email client, sending an email to the given `address` string
-    case email(_ name: String, address: String)
+    case email(_ label: String, address: String)
     /// Navigates the user to the apps AppStore page and writes a review (if available)
-    case review(_ name: String, appId: String)
+    case review(_ label: String, appId: String)
     /// Performs a custom action
-    case custom(_ name: String, action: () -> Void)
+    case custom(_ label: String, action: () -> Void)
 
     /// The `rawValue`
     private var rawValue: String {
@@ -29,7 +29,7 @@ public enum SentimentAlertAction: Identifiable {
         case .email(_, address: let address): return "email-\(address)"
         case .website(_, url: let url): return "website-\(url.absoluteString)"
         case .review(_, appId: let appId): return "review-\(appId)"
-        case .custom(let name, action: _): return name
+        case .custom(let label, action: _): return label
         }
     }
 
@@ -39,11 +39,11 @@ public enum SentimentAlertAction: Identifiable {
     /// The name to display in the alert for this `SentimentAlertAction` button
     public var name: String {
         switch self {
-        case .twitter(let name, handle: _): return name
-        case .email(let name, address: _): return name
-        case .website(let name, url: _): return name
-        case .review(let name, appId: _): return name
-        case .custom(let name, action: _): return name
+        case .twitter(let label, handle: _): return label
+        case .email(let label, address: _): return label
+        case .website(let label, url: _): return label
+        case .review(let label, appId: _): return label
+        case .custom(let label, action: _): return label
         }
     }
 }
